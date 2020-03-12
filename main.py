2de2f923365491f8e2be4bcd8b5a900ec1f48d69
Jack Biddlecombe <jackbid@hotmail.co.uk>
2020-03-12 11:06:59 +0000

    User in-built pandas correlation function instead of own implementation of cosine sim

--- main.py
+++ main.py
@@ -8,43 +8,10 @@ user = '73BED0524191DFB94AB901D12413517F'
 item = 93593
 item2 = 100507
 
-# Find items similar to a particular item
-def findSimilarItems(item):
-    simiarities = []
-    
-    for index, row in user_item_matrix.iterrows():
-        if index == item: continue
-        simiarities.append((index, cosine_similarity(user_item_matrix.loc[item], user_item_matrix.loc[index])))
-
-    return simiarities
-
-# Find the cosine similarity between two items, i1 and i2
-def cosine_similarity(i1, i2):
-    
-    # Convert to array and remove nan values
-    np_i1 = i1.to_numpy()
-    np_i2 = i2.to_numpy()
-
-    np_i1[np.isnan(np_i1)] = 0
-    np_i2[np.isnan(np_i2)] = 0
-
-    # Calculate the squared sums of np_i1 and np_i2
-    squarer = lambda t: t ** 2
-    vfunc = np.vectorize(squarer)
-
-    i1_squared_sum = sum(vfunc(np_i1))
-    i2_squared_sum = sum(vfunc(np_i2))
-
-    # Apple cosine similarity function
-    similarity = 0
-    for i in range(len(np_i1)):
-        similarity += (np_i1[i] * np_i2[i]) / (math.sqrt(i1_squared_sum) * math.sqrt(i2_squared_sum))
-
-    return similarity
 
 def readData():
     n = 14176 #number of records in file
-    s = 14000 #desired sample size
+    s = 14176 #desired sample size
     filename = "res/data.csv"
     skip = sorted(random.sample(range(n),n-s))
 
@@ -61,20 +28,41 @@ def preProcessData(rawData):
                             'ItemID': ratings['ItemID_x'],
                             'Rating': ratings['Rating_mean']})
 
-    result = pd.pivot_table(result, index='ItemID', columns='UserID', values='Rating').fillna(0)
+    result = pd.pivot_table(result, index='UserID', columns='ItemID', values='Rating').fillna(0)
 
     return result
 
+def findSimilarItems(item, ratings_mean_count):
+
+    item_series = user_item_matrix[item]
+    similar_items = user_item_matrix.corrwith(item_series, method='pearson')
+
+    corr_similar_items = pd.DataFrame(similar_items, columns=['Correlation'])
+    corr_similar_items.dropna(inplace=True)
+    #corr_similar_items = corr_similar_items.sort_values('Correlation', ascending=False).head()
+    corr_similar_items = corr_similar_items.join(ratings_mean_count['RatingCount'])
+    corr_similar_items = corr_similar_items[corr_similar_items['RatingCount']>10].sort_values('Correlation', ascending=False).head()
+    #corr_forrest_gump[corr_forrest_gump ['rating_counts']>50].sort_values('Correlation', ascending=False).head()
+    
+    return corr_similar_items 
 
 rawData = readData()
 user_item_matrix = preProcessData(rawData)
 
+ratings_mean_count = pd.DataFrame(rawData.groupby('ItemID')['Rating'].count().sort_values(ascending=False))
+ratings_mean_count = ratings_mean_count.rename(columns = {'ItemID': 'ItemID', 'Rating':'RatingCount'})
 
-s = findSimilarItems(224311)
+users = findSimilarItems(878115, ratings_mean_count)
+print(users)
+
+'''
+similarity_matrix = {}
+
+s = findSimilarItems(user_item_matrix.iloc[0])
 s.sort(key=operator.itemgetter(1), reverse=True)
 s = s[:10]
 print(s)
-
+'''
 
 
 
